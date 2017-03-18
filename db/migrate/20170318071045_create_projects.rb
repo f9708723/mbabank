fc22963e74409693aff775052a2d809e13b06977
NAGASHIMA-PC\f9708_000 <f9708723@hotmail.com>
2017-03-18 17:03:28 +0900

    changed migration file

--- db/migrate/20170318071045_create_projects.rb
+++ db/migrate/20170318071045_create_projects.rb
@@ -1,8 +1,12 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-
-      t.timestamps null: false
+      t.text :title
+      t.text :content
+      t.integer :price
+      t.date :start_day
+      t.date :end_day
+      t.timestamps
     end
   end
 end
