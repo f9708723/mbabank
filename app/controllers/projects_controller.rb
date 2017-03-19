df1f5f2b6063947c003158fd40f072bd9a4ee5da
NAGASHIMA-PC\f9708_000 <f9708723@hotmail.com>
2017-03-19 20:42:52 +0900

    added delete function

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,6 +11,11 @@ class ProjectsController < ApplicationController
     Project.create(title: project_params[:title], content: project_params[:content], price: project_params[:price], start_day: project_params[:start_day], end_day: project_params[:end_day])
   end
 
+  def destroy
+    project = Project.find(params[:id])
+     project.destroy
+  end
+
  private
   def project_params
     params.permit(:title, :content, :price, :start_day, :end_day)
