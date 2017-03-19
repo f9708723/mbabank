a79b3ea024420f3882d5bffe760e7d5db311de5f
NAGASHIMA-PC\f9708_000 <f9708723@hotmail.com>
2017-03-19 18:24:19 +0900

    completed new project entry and save at table

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,12 @@ class ProjectsController < ApplicationController
   def new
   end
 
+  def create
+    Project.create(title: project_params[:title], content: project_params[:content], price: project_params[:price], start_day: project_params[:start_day], end_day: project_params[:end_day])
+  end
+
+ private
+  def project_params
+    params.permit(:title, :content, :price, :start_day, :end_day)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
 root 'projects#index'
-resources :projects, only: [:new, :index]
+resources :projects, only: [:new, :index, :create]
 end
