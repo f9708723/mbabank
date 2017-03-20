90c9fe96a6375f63ba555dfebe3e5c395643fe12
NAGASHIMA-PC\f9708_000 <f9708723@hotmail.com>
2017-03-20 20:26:58 +0900

    added clients routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :clients
+devise_for :clients
+resources :clients, only: [:show, :edit, :update]
 root 'projects#index'
 resources :projects, only: [:new, :index, :create, :destroy, :edit, :update]
 end
