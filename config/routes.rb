057eb0552dfef29f04fceee4acc47897b7cc1a7b
NAGASHIMA-PC\f9708_000 <f9708723@hotmail.com>
2017-03-21 16:57:11 +0900

    completed toppage if client_sign_in?

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 devise_for :clients
-resources :clients, only: [:show, :edit, :update]
+resources :clients, only: [:show, :edit, :update, :index]
 root to: 'projects#index'
 resources :projects, only: [:new, :index, :create, :destroy, :edit, :update]
 end
