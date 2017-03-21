57f6ad3e99a92dd6b5cbeb77bc316053cbdb2342
NAGASHIMA-PC\f9708_000 <f9708723@hotmail.com>
2017-03-21 18:58:28 +0900

    completed edit and update of client show page

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,4 +2,16 @@ class ClientsController < ApplicationController
   def show
     @client = Client.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    current_client.update(update_params)
+  end
+
+  private
+  def update_params
+    params.require(:client).permit(:company_name, :division, :position, :family_name, :first_name)
+  end
 end
