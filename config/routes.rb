20ad4313c73a78ee47c7951740a8dd6f7c93ba35
NAGASHIMA-PC\f9708_000 <f9708723@hotmail.com>
2017-03-21 14:31:13 +0900

    completed log-in function

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
 devise_for :clients
 resources :clients, only: [:show, :edit, :update]
-root 'projects#index'
+root to: 'projects#index'
 resources :projects, only: [:new, :index, :create, :destroy, :edit, :update]
 end
